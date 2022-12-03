f0e3171adc4913f3a23fcda8af24fb73c9ad1d3e
Maksym Solomko <max.exc7@gmail.com>
2022-12-03 19:45:40 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import React from 'react'
+import { Feedback } from './Feedback/Feedback'
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template FEEDBACK))
-    </div>
-  );
-};
+    <>
+      <Feedback />
+    </>
+  )
+}
+
+// export const App = () => {
+//   <>
+//     <Feedback />  
+//     {/* console.log(<Feedback /> ) */}
+//   </>
+// }
+
+// export default  App;
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template FEEDBACK))
+//     </div>
+//   );
+// };
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Feedback = () => {
+    return (
+        <div>
+            <h1>Please leave feedback</h1>
+            <ul>                    
+                <button>Good</button>
+                <button>Neutral</button>
+                <button>Bad</button>
+            </ul>
+        </div>
+    )
+}
+
+// export default FeedBack;
